d2fb41c396925ba2e8d34622270ef551e37057e6
Gadfly <133796091+FeliceRivarez@users.noreply.github.com>
2024-12-21 23:12:50 +0800

    Starting 2D Lagrange

--- fluid-sim-master/code/fluid2d/Lagrangian/src/Solver.cpp
+++ fluid-sim-master/code/fluid2d/Lagrangian/src/Solver.cpp
@@ -5,13 +5,38 @@
 
 namespace FluidSimulation
 {
+    // 部分关键变量初始化
+    double mass;
+    double pi = 3.14159;
+    float supportRadius;
+    float supportRadiusSquare;
+    float supportRadiusCube;
+
 
     namespace Lagrangian2d
     {
+
         Solver::Solver(ParticleSystem2d &ps) : mPs(ps)
         {
+            mass= mPs.particleVolume * Lagrangian2dPara::density;
+            supportRadius = mPs.supportRadius;
+            supportRadiusSquare= mPs.supportRadius2;
+            supportRadiusCube = supportRadiusSquare * supportRadius;
+
         }
 
+        // 公式和理论推导参考：https://zhuanlan.zhihu.com/p/630175290        
+        // TODO: 领域搜索
+        std::vector<FluidSimulation::Lagrangian2d::ParticleInfo2d> searchNearArea(
+                                    const FluidSimulation::Lagrangian2d::ParticleInfo2d& p,
+                                    FluidSimulation::Lagrangian2d::ParticleSystem2d& mPs)
+        {
+
+        }
+
+        // TODO：密度计算
+
+        // TODO：压力计算
 
         void Solver::solve()
         {
